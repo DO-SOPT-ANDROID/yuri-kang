d3d8846f727f73a15a5b8ee7700578131956ce82
kangyuri1114 <yuri3334@gmail.com>
2023-10-21 00:27:48 +0900

    [ADD/#7] 더미 데이터 추가

--- app/src/main/java/org/sopt/dosopttemplate/data/dummyFriendsData.kt
+++ app/src/main/java/org/sopt/dosopttemplate/data/dummyFriendsData.kt
@@ -10,7 +10,7 @@ object dummyFriendsData {
         ),
         FriendsSealed.FriendsNomal(
             profileImage = R.drawable.img_default_kakao_profile,
-            name = "SOPT YB 김민우",
+            name = "SOPT YB 가나다",
             description = "떼잉",
         ),
         FriendsSealed.FriendsBirthday(
@@ -19,10 +19,58 @@ object dummyFriendsData {
             description = "생일 선물은 돈으로 주세용",
         ),
         FriendsSealed.FriendsMusic(
-            profileImage = R.drawable.img_default_kakao_profile,
-            name = "친구3...",
+            profileImage = R.drawable.img_danmuzi1,
+            name = "무즤다",
             description = "무식이즈 마이 라이프",
-            music = "뮤지크 - 가아수",
+            music = "네모의 꿈 - 가아수",
+        ),
+        FriendsSealed.FriendsNomal(
+            profileImage = R.drawable.img_default_kakao_profile,
+            name = "SOPT OB 박찬호",
+            description = "제가 LA에 있을때는 말이죠 정말 제가 꿈에 무대인 메이저리그로 진출해서 가는 식당마다 싸인해달라 기자들은 항상 붙어다니며 취재하고 제가 그 머~ 어~ 대통령이 된 기분이였어요 그런데 17일만에 17일만에 마이너리그",
+        ),
+        FriendsSealed.FriendsNomal(
+            profileImage = R.drawable.img_apeach,
+            name = "SOPT YB 어피치",
+            description = "복숭아 마싓당",
+        ),
+        FriendsSealed.FriendsMusic(
+            profileImage = R.drawable.img_danmuzi1,
+            name = "단무지",
+            description = "랑 짜장면",
+            music = "틀린그림찾기 - JAEHA",
+        ),
+        FriendsSealed.FriendsNomal(
+            profileImage = R.drawable.img_chunsik,
+            name = "SOPT YB 강춘식",
+            description = null,
+        ),
+        FriendsSealed.FriendsNomal(
+            profileImage = R.drawable.img_apeach,
+            name = "SOPT YB 어피치",
+            description = "복숭아 마싓당",
+        ),
+        FriendsSealed.FriendsNomal(
+            profileImage = R.drawable.img_prodo,
+            name = "SOPT YB 프로도",
+            description = null,
+        ),
+        FriendsSealed.FriendsNomal(
+            profileImage = R.drawable.img_lion3,
+            name = "SOPT YB 라이옹",
+            description = null,
+        ),
+        FriendsSealed.FriendsMusic(
+            profileImage = R.drawable.img_default_kakao_profile,
+            name = "SOPT YB 안봉이",
+            description = null,
+            music = "bad - wave to earth",
+        ),
+        FriendsSealed.FriendsMusic(
+            profileImage = R.drawable.img_default_kakao_profile,
+            name = "SOPT YB 미야옹",
+            description = "우린 오래 전부터~ ",
+            music = "Antifreeze - 검정치마",
         ),
     )
 }
