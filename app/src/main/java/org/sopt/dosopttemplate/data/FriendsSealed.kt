f0a3ff7a461bd88df63f64c3a2d1d24bb4194358
kangyuri1114 <yuri3334@gmail.com>
2023-10-21 00:21:27 +0900

    [MOD/#7] null 추가

--- app/src/main/java/org/sopt/dosopttemplate/data/FriendsSealed.kt
+++ app/src/main/java/org/sopt/dosopttemplate/data/FriendsSealed.kt
@@ -8,20 +8,20 @@ sealed class FriendsSealed {
 
     data class FriendsNomal(
         val name: String,
-        val description: String,
+        val description: String?,
         val profileImage: Int,
     ) : FriendsSealed()
 
     data class FriendsMusic(
         val name: String,
         val description: String,
-        val profileImage: Int,
+        val profileImage: Int?,
         val music: String
     ) : FriendsSealed()
 
     data class FriendsBirthday(
         val name: String,
         val description: String,
-        val profileImage: Int,
+        val profileImage: Int?,
     ) : FriendsSealed()
 }
