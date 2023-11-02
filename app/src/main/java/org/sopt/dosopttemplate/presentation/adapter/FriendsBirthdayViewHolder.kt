97bfcaac12bfb8f7cee58a843be756f09579f69c
kangyuri1114 <yuri3334@gmail.com>
2023-11-02 15:44:32 +0900

    [FEAT/#13] coil 라이브러리 적용

--- app/src/main/java/org/sopt/dosopttemplate/presentation/adapter/FriendsBirthdayViewHolder.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/adapter/FriendsBirthdayViewHolder.kt
@@ -1,6 +1,8 @@
 package org.sopt.dosopttemplate.presentation.adapter
 
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import org.sopt.dosopttemplate.R
 import org.sopt.dosopttemplate.data.FriendsSealed
 import org.sopt.dosopttemplate.databinding.ItemFriendsBirthdayBinding
 
@@ -9,7 +11,10 @@ class FriendsBirthdayViewHolder(private val binding: ItemFriendsBirthdayBinding)
 
     fun onBindView(friendsBirthdayData: FriendsSealed.FriendsBirthday) {
         binding.run {
-            ivFriendsBirthdayProfile.setImageResource(friendsBirthdayData.profileImage)
+            ivFriendsBirthdayProfile.load(friendsBirthdayData.profileImage) {
+                placeholder(R.drawable.img_default_kakao_profile)
+                error(R.drawable.img_default_kakao_profile) // Load Error 시 보여줄 이미지
+            }
             ivFriendsBirthdayProfile.clipToOutline = true
             tvFriendsBirthdayName.text = friendsBirthdayData.name
             tvFriendsBirthdayContent.text = friendsBirthdayData.description
--- app/src/main/java/org/sopt/dosopttemplate/presentation/adapter/FriendsMeViewHolder.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/adapter/FriendsMeViewHolder.kt
@@ -1,6 +1,8 @@
 package org.sopt.dosopttemplate.presentation.adapter
 
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import org.sopt.dosopttemplate.R
 import org.sopt.dosopttemplate.data.FriendsSealed
 import org.sopt.dosopttemplate.databinding.ItemFriendsMeBinding
 
@@ -9,7 +11,10 @@ class FriendsMeViewHolder(private val binding: ItemFriendsMeBinding) :
 
     fun onBindView(friendsMeData: FriendsSealed.FriendsMe) {
         binding.run {
-            ivFriendsMeProfile.setImageResource(friendsMeData.profileImage)
+            ivFriendsMeProfile.load(friendsMeData.profileImage) {
+                placeholder(R.drawable.img_default_kakao_profile)
+                error(R.drawable.img_default_kakao_profile) // Load Error 시 보여줄 이미지
+            }
             ivFriendsMeProfile.clipToOutline = true
             tvFriendsMeName.text = friendsMeData.name
         }
